476a5c60457ea40fc97ec708cb804605a9f6cdd6
xhd24 <xhd24@naver.com>
2025-09-23 14:54:24 +0900

    faq 끝

--- backend/routes/faq.js
+++ backend/routes/faq.js
@@ -1,11 +1,19 @@
 import {Router} from 'express';
-import { getPosts } from '../db.js';
+import { getQueryHx,addQuery } from '../db.js';
 
 const router = Router();
 
+//목록
 router.get('/',async(req,res)=>{
-    const posts = await getPosts();
+    const posts = await getQueryHx();
     res.json(posts);
-})
+});
+
+//글작석
+router.post("/write", async (req, res) => {
+  const { title, content, user_id } = req.body;
+  await addQuery(title, content, user_id);
+  res.json({ success: true });
+});
 
 export default router;
